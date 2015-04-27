93ba522f25016dca1c18e2ae6beedad5bfef88d5
Pedro Pena <p.pena3@umiami.edu>
2015-04-27 16:56:46 -0400

    Added route and view folder

--- app.js
+++ app.js
@@ -29,15 +29,22 @@ db.close();
 */
 
 var express = require('express');
+var path = require('path');
 var app = express();
-var cool = require('cool-ascii-faces');
+
+var routes = require('./routes/index');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
-  response.send(cool());
-});
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', routes);
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
+
+module.exports = app;
