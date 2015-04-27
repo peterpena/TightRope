867bbe404f9e39b7628fe9c08b9222a4c8946426
Pedro Pena <p.pena3@umiami.edu>
2015-04-27 16:09:07 -0400

    Test Node...

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ db.all("SELECT id AS Secondary, info FROM Art", function(err,rows){
 });
 
 db.close();
-*/
+
 
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('./Tightrope.db');
@@ -26,3 +26,18 @@ console.log(row.Secondary + " " + row.Career + ", " + row.Famous + "\n");
 
 db.close();
 
+*/
+
+var express = require('express');
+var app = express();
+var cool = require('cool-ascii-faces');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
