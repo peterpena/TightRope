802c514368e204d897a011003babc84ce24adf4f
Pedro Pena <p.pena3@umiami.edu>
2015-04-29 17:57:14 -0400

    Added PG logic

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,17 @@ router.get('/Sciences', function(req, res) {
     res.render('Sciences.jade', { title: 'Sciences' });
 });
 
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+})
 module.exports = router;
