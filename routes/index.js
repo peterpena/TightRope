6610e80acd19d0c669771388af63e4bb46b86007
dmorris422 <d.morris7@umiami.edu>
2015-04-29 17:47:42 -0400

    Fixed index.jade and added to index.js

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,17 @@ router.get('/', function(req, res) {
 router.get('/Sciences', function(req, res) {
     res.render('Sciences.jade', { title: 'Sciences' });
 });
+/* GET Arts */
+router.get('/Arts', function(req, res) {
+    res.render('Arts.jade', { title: 'Arts' });
+});
+/* GET Languages */
+router.get('/Languages', function(req, res) {
+    res.render('Languages.jade', { title: 'Languages' });
+});
+/* GET SocialStudies */
+router.get('/SocialStudies', function(req, res) {
+    res.render('SocialStudies.jade', { title: 'SocialStudies' });
+});
 
 module.exports = router;
