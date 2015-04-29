da6c78cae65b8014100ef5b473e7b54b17c8fb4b
Pedro Pena <p.pena3@umiami.edu>
2015-04-29 16:46:59 -0400

    face test

--- app.js
+++ app.js
@@ -1,33 +1,4 @@
 /*
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./../Downloads/Tightrope.db');
-
-//SELECT OPERATION
-db.all("SELECT id AS Secondary, info FROM Art", function(err,rows){
-    console.log(rows.info);
-});
-
-db.close();
-
-
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./Tightrope.db');
-
-
-var stmt = "SELECT Secondary, Career, Famous FROM Economics";
-db.each(stmt, function(err, row) {
-
-//
-// print out results
-//
-console.log(row.Secondary + " " + row.Career + ", " + row.Famous + "\n");
-
-});
-
-db.close();
-
-*/
-
 var express = require('express');
 var path = require('path');
 var app = express();
@@ -48,3 +19,18 @@ app.listen(app.get('port'), function() {
 });
 
 module.exports = app;
+*/
+
+var express = require('express');
+var app = express();
+var cool = require('cool-ascii-faces');
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running on port:" + app.get('port'))
+})
